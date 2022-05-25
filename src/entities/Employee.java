d87d796788c3b2b638be1d5b42ce54ffefb1aff9
aleff90 <thommy90vercetti@gmail.com>
2022-05-25 16:14:38 -0300

    entities package

---
+++ src/entities/Employee.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Employee {
+
+    public String name;
+    public double GrossSalary;
+    public double Tax;
+
+    public double NetSalary() {
+        return(0);
+    }
+
+    public void IncreaseSalary() {
+
+    }
+
+}
